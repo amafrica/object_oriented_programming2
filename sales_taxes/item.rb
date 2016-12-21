612264428eed900ff4f5c706fab7f2a76f02eb94
Andrea <andrea.africa93@gmail.com>
2016-12-21 13:39:54 -0500

    revisited sales tax exercise

--- sales_taxes/item.rb
+++ sales_taxes/item.rb
@@ -1,26 +1,59 @@
+# class Item
+#
+#   attr_reader :price
+#
+#   def initialize
+#     @price = price
+#   end
+#
+# end
+#
+#
+# class Sales < Item
+#   def basic_sales_tax
+#
+#
+#   end
+#
+# end
+#
+#  book = item.new("book", 12.49)
+#  music_cd = item.new("music_cd", 14.99)
+#  chocolate_bar = item.new("chocolate_bar", 0.85)
+
+# ASSIGNMENT REVISIT
 class Item
 
-  attr_reader :price
+  attr_accessor :name, :cost, :tax_exempt, :import_status, :import_tax, :base_sales_tax
 
-  def initialize
-    @price = price
+  def initialize(name, cost, tax_exempt, import_status, receipt)
+    @name = name
+    @cost = cost
+    @tax_exempt = tax_exempt
+    @import_status = import_status
+
+    @cost.to_f
+    receipt.item_list << self
+
+    set_base_sales_tax
+    set_import_tax
+
+  end
+
+  def set_base_sales_tax
+    if @tax_exempt
+      @base_sales_tax = 0
+    else
+      @base_sales_tax = @cost * 0.1
+    end
+  end
+
+  def set_import_tax
+    if @import_status == true
+      @import_tax = @cost * 0.05
+    else
+      @import_tax = 0
+    end
   end
 
 end
-
-
-class Sales < Item
-  def basic_sales_tax
-
-
-  end
-
-end
-
- book = item.new("book", 12.49)
- music_cd = item.new("music_cd", 14.99)
- chocolate_bar = item.new("chocolate_bar", 0.85)
-
-
-
- 
