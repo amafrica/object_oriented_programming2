9a7c24eccb4aff0a187ab526489cfdbc207c527e
Andrea <andrea.africa93@gmail.com>
2016-12-20 19:53:17 -0500

    revisited people exercise

--- people.rb
+++ people.rb
@@ -1,48 +1,99 @@
+# class Person
+#   attr_writer :name
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def greetings
+#     "Hi, my name is #{@name}"
+#   end
+#
+# end
+#
+#
+#
+# class Student < Person
+#
+#   def learn
+#     "I get it!"
+#   end
+#
+# end
+#
+#
+#
+# class Instructor < Person
+#
+#   def teach
+#     "Everything in Ruby is an Object."
+#   end
+#
+# end
+#
+#
+#
+#
+# chris = Instructor.new("Chris")
+# cristina = Student.new("Cristina")
+#
+#
+#
+# # Call greetings
+# puts chris.greetings
+# puts cristina.greetings
+#
+# # Call teach method
+# puts chris.teach
+# # Call learn method
+# puts cristina.learn
+
+## ASSIGNMENT REVISIT
+
+#4
 class Person
-  attr_writer :name
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
 
-  def greetings
-    "Hi, my name is #{@name}"
+#5
+  def greeting
+    puts "Sup! My name is #{name}."
   end
-
 end
 
 
-
+#1
 class Student < Person
 
+#2
   def learn
-    "I get it!"
+    puts "I get it!"
   end
 
 end
-
 
 
 class Instructor < Person
 
+#3
   def teach
-    "Everything in Ruby is an Object."
+    puts "Everything in Ruby is an Object."
   end
 
 end
 
-
-
-
+#6
 chris = Instructor.new("Chris")
+
+#7
 cristina = Student.new("Cristina")
 
 
+chris.greeting
+cristina.greeting
 
-# Call greetings
-puts chris.greetings
-puts cristina.greetings
-
-# Call teach method
-puts chris.teach
-# Call learn method
-puts cristina.learn
+#8
+chris.teach
+cristina.learn
--- sales_taxes/item.rb
+++ sales_taxes/item.rb
@@ -2,6 +2,9 @@ class Item
 
   attr_reader :price
 
+  def initialize
+    @price = price
+  end
 
 end
 
@@ -9,7 +12,15 @@ end
 class Sales < Item
   def basic_sales_tax
 
+
   end
 
 end
+
+ book = item.new("book", 12.49)
+ music_cd = item.new("music_cd", 14.99)
+ chocolate_bar = item.new("chocolate_bar", 0.85)
+
+
+
  
